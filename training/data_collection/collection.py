43534d298884a21b3b28436bbc86eba983845361
TianLangHin <tianlanghin@gmail.com>
2025-04-16 21:36:59 +1000

    Fixed image duplication and delayed camera problem

--- training/data_collection/collection.py
+++ training/data_collection/collection.py
@@ -10,7 +10,7 @@ category = input('Image folder: ')
 if not os.path.exists(os.path.join(os.getcwd(), category)):
     os.mkdir(category)
 
-ply_number = 1
+ply_number = 0
 while True:
     key = input(f'Move {ply_number} image: ')
     if key == 'quit':
@@ -20,5 +20,5 @@ while True:
     if not ret:
         print('End')
         break
-    cv2.imwrite(os.path.join(os.getcwd(), category, f'{ply_number}.png'), frame)
+    cv2.imwrite(os.path.join(os.getcwd(), category, f'{ply_number:>04}.png'), frame)
     ply_number += 1
